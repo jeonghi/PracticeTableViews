0264042f9d36cf8cd856070f2b16bbad636f15ab
jeonghi <jpark0902@kookmin.ac.kr>
2024-01-07 18:09:55 +0900

    Initial Commit

---
+++ PracticeTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTableView
+//
+//  Created by 쩡화니 on 1/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
