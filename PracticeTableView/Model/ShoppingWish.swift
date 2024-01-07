5bee829bffbdd250a3de5b56efdb8ea8b52c676b
jeonghi <jpark0902@kookmin.ac.kr>
2024-01-08 00:38:10 +0900

    [Add] 쇼핑 위시리스트 모델 정의

---
+++ PracticeTableView/Model/ShoppingWish.swift
@@ -0,0 +1,16 @@
+//
+//  Schdule.swift
+//  PracticeTableView
+//
+//  Created by 쩡화니 on 1/7/24.
+//
+
+import Foundation
+
+struct ShoppingWish: Identifiable, Codable {
+  var id: UUID = .init()
+  var isChecked: Bool = false
+  var content: String
+  var isImportant: Bool = false
+  var createdAt: Date = Date()
+}
---
+++ PracticeTableView/Storage/UserDefaultWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultWrapper.swift
+//  PracticeTableView
+//
+//  Created by 쩡화니 on 1/7/24.
+//
+
+import Foundation
